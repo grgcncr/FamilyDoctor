c07710b8a98aa851a62c58d9977d55edb019b04d
ThanosZappas <it2021030@hua.gr>
2023-12-14 21:21:17 +0200

    Starting Springboot Code

---
+++ src/main/java/Team70/FamilyDoctor/FamilyDoctorApplication.java
@@ -0,0 +1,13 @@
+package Team70.FamilyDoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Team70/FamilyDoctor/FamilyDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package Team70.FamilyDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
