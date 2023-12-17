ca5020f60aaea95da2a9083f76d17a1ae7e64c71
grgcncr <it2021032@hua.gr>
2023-12-17 20:41:31 +0200

    rahh

--- src/main/java/Team23/FamilyDoctor/FamilyDoctorApplication.java
+++ src/main/java/Team23/FamilyDoctor/FamilyDoctorApplication.java
@@ -2,8 +2,9 @@ package Team23.FamilyDoctor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class FamilyDoctorApplication {
 
 	public static void main(String[] args) {
